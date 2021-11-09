2a860257342d6b23ede30693aa267113cfc8d37b
sebnorth <sebnorth@gmail.com>
2021-11-09 13:57:19 +0100

    Dodaj pliki projektów.

---
+++ LinkedLists/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedLists
+{
+    /// <summary>
+    /// Tests the linked lists
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Tests the linked lists
+        /// </summary>
+        /// <param name="args">command-line arguments</param>
+        static void Main(string[] args)
+        {
+            //TestUnsortedLinkedList.RunTestCases();
+            //TestSortedLinkedList.RunTestCases();
+
+            UnsortedLinkedList<int> lista = new UnsortedLinkedList<int>();
+            lista.Add(100);
+            lista.Add(200);
+            Console.WriteLine(lista);
+
+            Console.ReadKey();
+        }
+    }
+}
